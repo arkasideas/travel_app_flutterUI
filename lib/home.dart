52bf75e505d72cf0b399e3e8ad8312c057c0a4f6
arkasideas <50776482+arkasideas@users.noreply.github.com>
2023-08-09 07:40:59 +0700

    update 8 agustus 2023

--- lib/home.dart
+++ lib/home.dart
@@ -4,9 +4,53 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.white,
       appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
         actions: [
-          Icon(Icons.menu),
+          Padding(
+            padding: const EdgeInsets.only(right: 10),
+            child: Icon(
+              Icons.menu,
+              color: Colors.black,
+            ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(left: 15),
+            child: Text(
+              'Travel Blog',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 36,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Most Popular',
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                Text(
+                  'View All',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.deepOrange,
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
